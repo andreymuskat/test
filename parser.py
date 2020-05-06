69d2b158d1f4526dd1d4b97866eae9636b9dfaf7
andreymuskat <54885364+andreymuskat@users.noreply.github.com>
2020-05-06 17:06:05 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -10,11 +10,17 @@ soup = BS(full_page.content, 'html.parser')
 content = soup.findAll("tr")
 
 f = open(r"C:\Users\User\Desktop\Курс.txt", "w")
-
+f.close()
 i=0
 while i<34:
-    i=i+1
     f = open(r"C:\Users\User\Desktop\Курс.txt", "a")
     f.write(content[i].text + '\n' + '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' + '\n')
     f.close()
     print(content[i].text)
+    i=i+1
+file = open(r"C:\Users\User\Desktop\Курс.txt", "r")
+print (file.read())
+file.close()
+import os
+file_delete = "C:/Users/User/Desktop/Курс.txt"
+os.remove(file_delete)    
